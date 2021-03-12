dca8c9d5db3b7e7b525f6ca60dcce7b594d4ca01
Anand Chitipothu <anandology@gmail.com>
2021-03-12 05:37:24 +0000

    Added thumbnails to the recent sketches page.
    - Added an svg field to store the thumbnail
    - The thumbnail is computed and cached in redis

--- community/lms/doctype/lms_sketch/lms_sketch.py
+++ community/lms/doctype/lms_sketch/lms_sketch.py
@@ -3,12 +3,43 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import hashlib
+from urllib.parse import urlparse
 import frappe
 from frappe.model.document import Document
+from . import livecode
 
 class LMSSketch(Document):
     def get_owner_name(self):
         return get_userinfo(self.owner)['full_name']
+
+    def get_livecode_url(self):
+        doc = frappe.get_cached_doc("LMS Settings")
+        return doc.livecode_url
+
+    def get_livecode_ws_url(self):
+        url = urlparse(self.get_livecode_url())
+        protocol = "wss" if url.scheme == "https" else "ws"
+        return protocol + "://" + url.netloc + "/livecode"
+
+    def to_svg(self):
+        return self.svg or self.render_svg()
+
+    def render_svg(self):
+        h = hashlib.md5(self.code.encode('utf-8')).hexdigest()
+        cache = frappe.cache()
+        key = "sketch-" + h
+        value = cache.get(key)
+        if value:
+            value = value.decode('utf-8')
+        else:
+            ws_url = self.get_livecode_ws_url()
+            value = livecode.livecode_to_svg(ws_url, self.code)
+            cache.set(key, value)
+        return value
+
+    def __repr__(self):
+        return f"<LMSSketch {self.name}>"
 
 @frappe.whitelist()
 def save_sketch(name, title, code):
@@ -29,6 +60,7 @@ def save_sketch(name, title, code):
             }
         doc.title = title
         doc.code = code
+        doc.svg = ''
         doc.save()
         status = "updated"
     return {
@@ -50,13 +82,13 @@ def get_recent_sketches():
     """
     sketches = frappe.get_all(
         "LMS Sketch",
-        fields=['name', 'title', 'owner', 'modified'],
+        fields='*',
         order_by='modified desc',
         page_length=100
     )
     for s in sketches:
         s['owner_name'] = get_userinfo(s['owner'])['full_name']
-    return sketches
+    return [frappe.get_doc(doctype='LMS Sketch', **doc) for doc in sketches]
 
 def get_userinfo(email):
     """Returns the username and fullname of a user.
