7b142fd72db73856c9112407e3838b4f8e15cadd
pateljannat <pateljannat2308@gmail.com>
2021-04-28 13:50:40 +0530

    test: member creation from user

--- community/community/doctype/community_member/test_community_member.py
+++ community/community/doctype/community_member/test_community_member.py
@@ -2,11 +2,16 @@
 # Copyright (c) 2021, Frappe and Contributors
 # See license.txt
 from __future__ import unicode_literals
-
-# import frappe
+from community.lms.doctype.lms_course.test_lms_course import new_user
+import frappe
 import unittest
 
 class TestCommunityMember(unittest.TestCase):
 	
-	def test_member_created_from_user():
-		pass
+	def test_member_created_from_user(self):
+		user = new_user("Test User", "test_user@example.com")
+		member = frappe.get_doc("Community Member", {"email": "test_user@example.com"})
+		self.assertEqual(user.full_name, member.full_name)
+		self.assertEqual(member.owner, user.email)
+		self.assertEqual(user.username, member.username)
+		self.assertEqual(member.username, member.route)
