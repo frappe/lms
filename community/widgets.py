4874d99e44eb2eb540b541c2d7280512abe43530
Anand Chitipothu <anandology@gmail.com>
2021-04-29 10:37:36 +0530

    feat: widgets interface
    Widgets are reusable jinja templates which can be used in other
    themplates. Widgets are written in widgets/ directory in every frappe
    module and can be accessed as `{{ widgets.WidgetName(...) }}` from any
    template.

---
+++ community/widgets.py
@@ -0,0 +1,60 @@
+"""The widgets provides access to HTML widgets
+provided in each frappe module.
+
+Widgets are simple moduler templates that can reused
+in multiple places. These are like macros, but accessing
+them will be a lot easier.
+
+The widgets will be provided
+"""
+import frappe
+from frappe.utils.jinja import get_jenv
+
+# search path for widgets.
+# When {{widgets.SomeWidget()}} is called, it looks for
+# widgets/SomeWidgets.html in each of these modules.
+MODULES = [
+    "lms"
+]
+
+def update_website_context(context):
+    """Adds widgets to the context.
+
+    Called from hooks.
+    """
+    context.widgets = Widgets()
+
+class Widgets:
+    """The widget collection.
+
+    This is just a placeholder object and returns the appropriate
+    widget when accessed using attribute.
+
+        >>> widgets = Widgets()
+        >>> widgets.HelloWorld(name="World!")
+        '<div>Hello, World!</div>'
+    """
+    def __getattr__(self, name):
+        if not name.startswith("__"):
+            return Widget(name)
+        else:
+            raise AttributeError(name)
+
+class Widget:
+    """The Widget class renders a widget.
+
+    Widget is a reusable template defined in widgets/ directory in
+    each frappe module.
+
+        >>> w = Widget("HelloWorld")
+        >>> w(name="World!")
+        '<div>Hello, World!</div>'
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, **kwargs):
+        # the widget could be in any of the modules
+        paths = [f"{module}/widgets/{self.name}.html" for module in MODULES]
+        env = get_jenv()
+        return env.get_or_select_template(paths).render(kwargs)
