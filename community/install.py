e330f45adcc8539d0f35a7e6c942b77aaa89f6c9
Anand Chitipothu <anandology@gmail.com>
2021-05-05 12:53:24 +0530

    style: fixed the footer

--- community/install.py
+++ community/install.py
@@ -6,6 +6,7 @@ import frappe
 def after_install():
     set_app_name()
     disable_signup()
+    add_footer_items()
 
 def set_app_name():
     app_name = os.getenv("FRAPPE_APP_NAME")
@@ -14,3 +15,15 @@ def set_app_name():
 
 def disable_signup():
     frappe.db.set_value("Website Settings", None, "disable_signup", 1)
+
+def add_footer_items():
+    items = [
+        {"label": "About", "url": "/about"},
+        {"label": "Blog", "url": "/blog"},
+        {"label": "Github", "url": "https://github.com/fossunited/community"}
+    ]
+    doc = frappe.get_doc("Website Settings", None)
+    doc.update({
+        "footer_items": items
+    })
+    doc.save()
