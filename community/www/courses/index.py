29f9141ad83ed5d51e705acc2c32499bb743bfb6
pateljannat <pateljannat2308@gmail.com>
2021-05-19 13:16:46 +0530

    fix: removed slug field ref from courses page

--- community/www/courses/index.py
+++ community/www/courses/index.py
@@ -3,10 +3,11 @@ import frappe
 def get_context(context):
     context.no_cache = 1
     context.courses = get_courses()
+    print(context)
 
 def get_courses():
     courses = frappe.get_all(
         "LMS Course",
-        fields=['name', 'slug', 'title', 'description']
+        fields=['name', 'title', 'description']
     )
     return courses
