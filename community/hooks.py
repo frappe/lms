c77835b81f4b96576efcc5d2e6342c3ac4efac6b
Anand Chitipothu <anandology@gmail.com>
2021-05-21 13:13:34 +0530

    feat: added page to see progress of a batch

--- community/hooks.py
+++ community/hooks.py
@@ -147,6 +147,7 @@ primary_rules = [
     {"from_route": "/courses/<course>/<batch>/members", "to_route": "batch/members"},
     {"from_route": "/courses/<course>/<batch>/discuss", "to_route": "batch/discuss"},
     {"from_route": "/courses/<course>/<batch>/about", "to_route": "batch/about"},
+    {"from_route": "/courses/<course>/<batch>/progress", "to_route": "batch/progress"}
 ]
 
 # Any frappe default URL is blocked by profile-rules, add it here to unblock it
