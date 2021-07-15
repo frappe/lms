5ecae0df6178220b14d13aeffa20ff1fe64e5a02
pateljannat <pateljannat2308@gmail.com>
2021-07-15 17:01:15 +0530

    fix: removed unused pages

--- community/www/batch/home.py
+++
@@ -1,5 +0,0 @@
-import frappe
-from . import utils
-
-def get_context(context):
-    utils.get_common_context(context)
--- community/www/batch/members.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from . import utils
-
-def get_context(context):
-    utils.get_common_context(context)
-    if not context.membership:
-        utils.redirect_to_lesson(context.course)
