44ca940c6ba7f9115291a32da2184a66966631d9
pateljannat <pateljannat2308@gmail.com>
2021-08-03 18:30:52 +0530

    fix: quiz enhancements and tests

--- community/lms/doctype/lms_quiz/test_lms_quiz.py
+++ community/lms/doctype/lms_quiz/test_lms_quiz.py
@@ -3,6 +3,39 @@
 
 # import frappe
 import unittest
+import frappe
 
 class TestLMSQuiz(unittest.TestCase):
-	pass
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        frappe.get_doc({
+            "doctype": "LMS Quiz",
+            "title": "Test Quiz"
+        }).save()
+
+    def test_with_multiple_options(self):
+        quiz = frappe.get_doc("LMS Quiz", "Test Quiz")
+        quiz.append("questions", {
+            "question": "Question multiple",
+            "option_1": "Option 1",
+            "is_correct_1": 1,
+            "option_2": "Option 2",
+            "is_correct_2": 1
+        })
+        quiz.save()
+        self.assertTrue(quiz.questions[0].multiple)
+
+    def test_with_no_correct_option(self):
+        quiz = frappe.get_doc("LMS Quiz", "Test Quiz")
+        quiz.append("questions", {
+            "question": "Question no correct option",
+            "option_1": "Option 1",
+            "option_2": "Option 2",
+        })
+        self.assertRaises(frappe.ValidationError, quiz.save)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        frappe.db.delete("LMS Quiz", "Test Quiz")
+        frappe.db.delete("LMS Quiz Question", {"parent": "Test Quiz"})
