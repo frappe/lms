844fcc9bcab04163220b97d03f9f6b92925d1f03
pateljannat <pateljannat2308@gmail.com>
2021-08-06 14:41:11 +0530

    fix: username validations

--- community/lms/web_form/profile/profile.js
+++ community/lms/web_form/profile/profile.js
@@ -1,7 +1,14 @@
 frappe.ready(function () {
+
   frappe.web_form.after_load = () => {
     if (!frappe.utils.get_url_arg("name")) {
       window.location.href = `/edit-profile?name=${frappe.session.user}`;
     }
+  }
+
+  frappe.web_form.after_save = () => {
+    setTimeout(() => {
+      window.location.href = `/${frappe.web_form.get_value(["username"])}`;
+    })
   }
 })
