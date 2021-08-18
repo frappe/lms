7e5e167eec31c513505223dd82cef75df7074cd8
Summayya <frappe@Summayyas-MacBook-Air.local>
2021-08-18 19:40:35 +0530

    fix: remove dummy data

--- community/event_management/doctype/speaker_registration/speaker_registration.py
+++ community/event_management/doctype/speaker_registration/speaker_registration.py
@@ -7,5 +7,4 @@ from frappe.model.document import Document
 class SpeakerRegistration(Document):
 	
 	def on_update(self):
-		abc = frappe.get_doc("Speaker Registration", 'SPEAKER-REGIS0001')
-		print(type(abc))
+		pass
