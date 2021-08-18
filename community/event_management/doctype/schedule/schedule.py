eb58b1c149281b3b3ad16e0874484c8ffc5cd0c8
Summayya <frappe@Summayyas-MacBook-Air.local>
2021-08-18 23:53:02 +0530

    fix: remove hover

--- community/event_management/doctype/schedule/schedule.py
+++ community/event_management/doctype/schedule/schedule.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2021, FOSS United and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Schedule(Document):
-	pass
+	def before_save(self):
+		exists = frappe.db.exists(
+            "Schedule",
+            {
+                "Event": self.event,
+                "slot": self.slot
+            },
+        )
+
+		if exists:
+			frappe.throw("Slot already Assigned")
--- community/event_management/doctype/slot/slot.py
+++ community/event_management/doctype/slot/slot.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2021, FOSS United and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 class Slot(Document):
-	pass
+
+	def before_save(self):
+		event = frappe.get_doc("Event Details", self.event)
+		if getdate(self.date) < event.start_date or getdate(self.date) > event.end_date:
+			frappe.throw("Slot should be in Event's span") 
+
