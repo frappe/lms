f61c5a2fa15af1ab31693b4d4f4c1e16b6bf8694
Summayya <frappe@Summayyas-MacBook-Air.local>
2021-08-24 17:47:58 +0530

    fix: implicit user in speaker and exhibitor

--- community/event_management/web_form/speaker_registration/speaker_registration.js
+++ community/event_management/web_form/speaker_registration/speaker_registration.js
@@ -1,4 +1,7 @@
 frappe.ready(function () {
+	frappe.web_form.after_load = () => {
+		frappe.web_form.set_value("user", frappe.session.user);
+	  }
 	frappe.web_form.after_save = () => {
 		setTimeout(function () {
 			window.location.href = '/event/conference2021/propose-talk';
