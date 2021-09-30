ebc3cf1cbf0ec598c0266a12484a0025e365a217
pateljannat <pateljannat2308@gmail.com>
2021-09-30 18:31:41 +0530

    feat: live and upcoming course headers

--- community/www/courses/index.py
+++ community/www/courses/index.py
@@ -2,7 +2,7 @@ import frappe
 
 def get_context(context):
     context.no_cache = 1
-    context.courses = get_courses()
+    context.live_courses, context.upcoming_courses = get_courses()
     context.metatags = {
         "title": "All Courses",
         "image": frappe.db.get_single_value("Website Settings", "banner_image"),
@@ -11,8 +11,14 @@ def get_context(context):
     }
 
 def get_courses():
-    course_names = frappe.get_all("LMS Course", filters={"is_published": True}, order_by="upcoming", pluck="name")
-    courses = []
+    course_names = frappe.get_all("LMS Course",
+                                filters={"is_published": True},
+                                fields=["name", "upcoming"])
+
+    live_courses, upcoming_courses = [], []
     for course in course_names:
-        courses.append(frappe.get_doc("LMS Course", course))
-    return courses
+        if course.upcoming:
+            upcoming_courses.append(frappe.get_doc("LMS Course", course.name))
+        else:
+            live_courses.append(frappe.get_doc("LMS Course", course.name))
+    return live_courses, upcoming_courses
