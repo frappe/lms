857f1700349228946dc09e8426e913489b68b98d
Jannat Patel <pateljannat2308@gmail.com>
2021-10-26 16:07:22 +0530

    fix: help section in course lesson

--- school/lms/doctype/course_lesson/course_lesson.js
+++ school/lms/doctype/course_lesson/course_lesson.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, FOSS United and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Lesson', {
+frappe.ui.form.on('Course Lesson', {
   setup: function (frm) {
     frm.trigger('setup_help');
   },
