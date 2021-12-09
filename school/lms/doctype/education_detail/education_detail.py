fd524f627e2a7114a8bb89fb60630c0576472ecf
Jannat Patel <pateljannat2308@gmail.com>
2021-12-09 19:07:04 +0530

    fix: profile web form

--- school/lms/doctype/education_detail/education_detail.py
+++ school/lms/doctype/education_detail/education_detail.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class EducationDetails(Document):
+class EducationDetail(Document):
 	pass
--- school/lms/web_form/profile/profile.js
+++ school/lms/web_form/profile/profile.js
@@ -1,10 +1,35 @@
 frappe.ready(function () {
 
   frappe.web_form.after_load = () => {
+
     if (!frappe.utils.get_url_arg("name")) {
       window.location.href = `/edit-profile?name=${frappe.session.user}`;
     }
+
+    $(document).on("click", "input[data-fieldname='current']", (e) => {
+      if ($(e.currentTarget).prop("checked"))
+        $("div[data-fieldname='to_date']").addClass("hide");
+      else
+        $("div[data-fieldname='to_date']").removeClass("hide");
+    });
+
   }
+
+  frappe.web_form.validate = () => {
+    let to_date_missing;
+    const data = frappe.web_form.get_values();
+    data.work_experience.forEach(exp => {
+      if (!exp.current && !exp.to_date) {
+        to_date_missing = true
+        frappe.msgprint('To Date is mandatory in Work Experience.');
+      }
+    });
+    if (to_date_missing)
+      return false;
+    return true;
+  };
+
+
 
   frappe.web_form.after_save = () => {
     setTimeout(() => {
