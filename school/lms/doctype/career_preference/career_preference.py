8489b1d9436aa010bedf9961e32ac06577675261
Jannat Patel <pateljannat2308@gmail.com>
2021-12-14 16:46:06 +0530

    fix: text editor for bio and profile redirect changes

--- school/lms/doctype/career_preference/career_preference.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, Frappe and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class CareerPreference(Document):
-	pass
