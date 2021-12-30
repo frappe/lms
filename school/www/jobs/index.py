aa8142618969499b28b427da4531ef53b42e94e4
Jannat Patel <pateljannat2308@gmail.com>
2021-12-30 11:51:27 +0530

    fix: show jobs with Open status

--- school/www/jobs/index.py
+++ school/www/jobs/index.py
@@ -3,10 +3,12 @@ import frappe
 def get_context(context):
     context.jobs = frappe.get_all("Job Opportunity",
         {
-            "status": "Approved"
+            "status": "Approved",
+            "job_opportunity_status": "Open"
         },
         [
             "job_title", "location", "type", "company_name",
             "company_logo", "name"
-        ])
+        ],
+        order_by="creation desc")
 
