28ef8e7db7906d1214d51e7d9e85651a01c85bc7
Jannat Patel <pateljannat2308@gmail.com>
2022-02-21 11:43:05 +0530

    feat: quiz redesign

--- school/hooks.py
+++ school/hooks.py
@@ -184,7 +184,8 @@ jinja = {
         "school.lms.utils.is_eligible_to_review",
         "school.lms.utils.get_initial_members",
         "school.lms.utils.get_sorted_reviews",
-        "school.lms.utils.is_instructor"
+        "school.lms.utils.is_instructor",
+        "school.lms.utils.convert_number_to_character"
     ],
     "filters": []
 }
