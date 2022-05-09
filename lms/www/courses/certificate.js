91dc99d8ef12177dd871b59ec53f0f8908ac0c7b
Jannat Patel <pateljannat2308@gmail.com>
2022-05-09 13:17:20 +0530

    fix: certificate custom and style

--- lms/www/courses/certificate.js
+++ lms/www/courses/certificate.js
@@ -1,16 +1,16 @@
 frappe.ready(() => {
 
-  $("#export-as-pdf").click((e) => {
-    export_as_pdf(e);
-  })
+    $("#export-as-pdf").click((e) => {
+        export_as_pdf(e);
+    })
 
-})
+});
 
-var export_as_pdf = (e) => {
-  var button = $(e.currentTarget);
-  button.text(__("Exporting..."));
+const export_as_pdf = (e) => {
+    let button = $(e.currentTarget);
+    button.text(__("Exporting..."));
 
-  html2canvas(document.querySelector('.common-card-style'), {
+  html2canvas(document.querySelector('.certificate-card'), {
     scrollY: -window.scrollY,
     scrollX: 0
   }).then(function(canvas) {
