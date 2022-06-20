5e875b6d71353eab805181c11383c07aa4647557
Jannat Patel <pateljannat2308@gmail.com>
2022-06-20 17:44:14 +0530

    refactor: member field in lms quiz submission

---
+++ lms/patches/v0_0/quiz_submission_member.py
@@ -0,0 +1,8 @@
+import frappe
+
+def execute():
+    frappe.reload_doc("lms", "doctype", "lms_quiz_submission")
+    submissions = frappe.db.get_all("LMS Quiz Submission", fields=["name", "owner"])
+
+    for submission in submissions:
+        frappe.db.set_value("LMS Quiz Submission", submission.name, "member", submission.owner)
