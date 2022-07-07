d20f932316bc7a34c52542243e2118c24bf6bb8f
Jannat Patel <pateljannat2308@gmail.com>
2022-07-07 17:08:53 +0530

    fix: delete webforms of unused modules

--- lms/patches/v0_0/delete_old_module_docs.py
+++ lms/patches/v0_0/delete_old_module_docs.py
@@ -5,3 +5,11 @@ def execute():
     frappe.db.delete("DocType", {"module": "Conference"})
     frappe.db.delete("DocType", {"module": "Hackathon"})
     frappe.db.delete("DocType", {"module": "Event Management"})
+
+    frappe.db.delete("Web Form", {"module": "Conference"})
+    frappe.db.delete("Web Form", {"module": "Hackathon"})
+    frappe.db.delete("Web Form", {"module": "Event Management"})
+
+    frappe.db.delete("Module Def", "Conference")
+    frappe.db.delete("Module Def", "Hackathon")
+    frappe.db.delete("Module Def", "Event Management")
