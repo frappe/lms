7ef625c59800b1a4a6f7ef258e89f2aefb542acf
Jannat Patel <pateljannat2308@gmail.com>
2022-09-05 11:35:21 +0530

    chore: logging user

--- lms/lms/doctype/lms_course/lms_course.js
+++ lms/lms/doctype/lms_course/lms_course.js
@@ -3,31 +3,31 @@
 
 frappe.ui.form.on('LMS Course', {
 
-  onload: function (frm) {
+    onload: function (frm) {
 
-    frm.set_query("chapter", "chapters", function () {
-      return {
-        filters: {
-          "course": frm.doc.name,
-        }
-      };
-    });
+        frm.set_query("chapter", "chapters", function () {
+            return {
+                filters: {
+                    "course": frm.doc.name,
+                }
+            };
+        });
 
-    frm.set_query("instructor", function (doc) {
-      return {
-        filters: {
-          "ignore_user_type": 1,
-        }
-      };
-    });
+        frm.set_query("instructor", "instructors", function () {
+            return {
+                filters: {
+                    "ignore_user_type": 1,
+                }
+            };
+        });
 
-    frm.set_query("course", "related_courses", function () {
-      return {
-        filters: {
-          "published": true,
-        }
-      };
-    });
-  }
+        frm.set_query("course", "related_courses", function () {
+            return {
+                filters: {
+                    "published": true,
+                }
+            };
+        });
 
+    }
 });
