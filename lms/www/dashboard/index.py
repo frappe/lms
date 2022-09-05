3a08f57fb1aa163e0dc5463758c1f16748fb1b96
Jannat Patel <pateljannat2308@gmail.com>
2022-09-05 14:53:13 +0530

    fix: context for dashboard

--- lms/www/dashboard/index.py
+++ lms/www/dashboard/index.py
@@ -2,7 +2,7 @@ import frappe
 from lms.lms.utils import has_course_instructor_role
 
 
-def get_content(context):
+def get_context(context):
     context.no_cache = 1
     portal_course_creation = frappe.db.get_single_value("LMS Settings", "portal_course_creation")
     context.show_creators_section = portal_course_creation == "Anyone" or has_course_instructor_role()
