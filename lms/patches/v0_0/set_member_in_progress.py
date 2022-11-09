94e7f6a1f47d898f2112c4b9d89b1850bb78d0b1
Jannat Patel <pateljannat2308@gmail.com>
2022-11-09 15:21:08 +0530

    feat: course and lesson completion count

---
+++ lms/patches/v0_0/set_member_in_progress.py
@@ -0,0 +1,9 @@
+import frappe
+
+def execute():
+	progress_records = frappe.get_all("LMS Course Progress", fields=["name", "owner"])
+
+	for progress in progress_records:
+		full_name = frappe.db.get_value("User", progress.owner, "full_name")
+		frappe.db.set_value("LMS Course Progress", progress.name, "member", progress.owner)
+		frappe.db.set_value("LMS Course Progress", progress.name, "member_name", full_name)
