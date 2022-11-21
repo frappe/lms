b5078c1f1f70d20f2829790d845409ad56db7815
Jannat Patel <pateljannat2308@gmail.com>
2022-11-21 13:36:37 +0530

    fix: removed unused file

--- lms/www/courses/index.js
+++
@@ -1,9 +0,0 @@
-frappe.ready(() => {});
-
-const change_hash = (e) => {
-	window.location.hash = $(e.currentTarget).attr("href");
-};
-
-const open_tab = () => {
-	$(`a[href="${window.location.hash}"]`).click();
-};
