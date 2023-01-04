ec31c96120643a8a1e7db1e170f30c079808d5d5
Jannat Patel <pateljannat2308@gmail.com>
2023-01-04 16:29:07 +0530

    fix: linters

--- lms/patches/v0_0/user_singles_issue.py
+++ lms/patches/v0_0/user_singles_issue.py
@@ -1,7 +1,10 @@
 import frappe
 
+
 def execute():
-	rows = frappe.db.sql("select field from `tabSingles` where doctype='User'", as_dict = True)
+	rows = frappe.db.sql(
+		"select field from `tabSingles` where doctype='User'", as_dict=True
+	)
 
 	if len(rows):
 		frappe.db.sql("delete from `tabSingles` where doctype='User'")
