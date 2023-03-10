c03cca21e8ab06c163c3977b5631199d3404b34f
Jannat Patel <jannatpatel@Jannats-MacBook-Air.local>
2023-03-10 23:08:00 +0530

    fix: redirection after completing setup wizard

--- lms/public/js/setup_wizard.js
+++ lms/public/js/setup_wizard.js
@@ -2,4 +2,4 @@ frappe.provide("lms.setup");
 
 // redirect to desk page 'lms' after setup wizard is complete
 // 'lms' desk page redirects to '/courses'
-frappe.setup.welcome_page = "/app/lms-home";
+//frappe.setup.welcome_page = "/app/lms-home";
