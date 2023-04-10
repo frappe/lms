0a91e5aa052ce705a7f87c423c9081330e971a31
Jannat Patel <pateljannat2308@gmail.com>
2023-04-10 22:35:33 +0530

    fix: reload question doctype in patch

--- lms/patches/v0_0/add_question_type.py
+++ lms/patches/v0_0/add_question_type.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("lms", "doctype", "lms_quiz_question")
 	questions = frappe.get_all("LMS Quiz Question", pluck="name")
 
 	for question in questions:
