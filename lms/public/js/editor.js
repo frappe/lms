f97ae4e4e2c050b7c626d83c39e40dc554247a67
Jannat Patel <pateljannat2308@gmail.com>
2023-04-21 09:58:59 +0530

    feat: adding editorjs

--- lms/public/js/editor.js
+++ lms/public/js/editor.js
@@ -1,5 +1,9 @@
 import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
 
-const editor = new EditorJS("body");
-
-console.log(editor);
+create_editor_for_short_description = () => {
+	let editor = new EditorJS({
+		holder: "course-description",
+	});
+};
