b8471dd753c9c94e83304e50b5a08f8386c7b227
Jannat Patel <pateljannat2308@gmail.com>
2023-05-03 20:55:49 +0530

    feat: paid classes and seat count

--- lms/www/classes/index.py
+++ lms/www/classes/index.py
@@ -8,13 +8,22 @@ def get_context(context):
 	context.is_moderator = has_course_moderator_role()
 	classes = frappe.get_all(
 		"LMS Class",
-		fields=["name", "title", "start_date", "end_date"],
+		fields=["name", "title", "start_date", "end_date", "paid_class", "seat_count"],
 	)
+
 	past_classes, upcoming_classes = [], []
 	for class_ in classes:
-		if getdate(class_.end_date) < getdate():
+		if class_.seat_count:
+			filled_seats = frappe.db.count(
+				"Class Student",
+				filters={"parent": class_.name},
+			)
+			class_.seats_left = class_.seat_count - filled_seats
+
+		if getdate(class_.start_date) < getdate():
 			past_classes.append(class_)
 		else:
 			upcoming_classes.append(class_)
+
 	context.past_classes = past_classes
 	context.upcoming_classes = upcoming_classes
