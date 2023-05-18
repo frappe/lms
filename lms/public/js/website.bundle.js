93f0f8ab4477b5b1de6c5a68994509c71ae1d4a2
Jannat Patel <pateljannat2308@gmail.com>
2023-05-18 14:26:48 +0530

    fix: telemetry capture on client side

--- lms/public/js/website.bundle.js
+++ lms/public/js/website.bundle.js
@@ -1,4 +1,3 @@
 import "./profile.js";
 import "./common_functions.js";
 import "../../../../frappe/frappe/public/js/frappe/ui/chart.js";
-import "../../../../frappe/frappe/public/js/telemetry/index.js";
