bc2dc679a8b26454f646f4fd3ed2cbd9435329b1
Jannat Patel <pateljannat2308@gmail.com>
2023-10-13 18:15:49 +0530

    fix: revert ci changes

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -2,10 +2,6 @@
 set -e
 cd ~ || exit
 
-sudo apt update
-sudo apt remove mysql-server mysql-client
-sudo apt install libcups2-dev redis-server mariadb-client-10.6
-
 echo "Setting Up Bench..."
 
 pip install frappe-bench
@@ -23,16 +19,14 @@ mkdir ~/frappe-bench/sites/lms.test
 cp "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/lms.test/site_config.json
 
 
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL character_set_server = 'utf8mb4'";
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL character_set_server = 'utf8mb4'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
 
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE DATABASE test_lms";
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE USER 'test_lms'@'localhost' IDENTIFIED BY 'test_lms'";
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "GRANT ALL PRIVILEGES ON \`test_lms\`.* TO 'test_lms'@'localhost'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE DATABASE test_lms";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE USER 'test_lms'@'localhost' IDENTIFIED BY 'test_lms'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "GRANT ALL PRIVILEGES ON \`test_lms\`.* TO 'test_lms'@'localhost'";
 
-mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "FLUSH PRIVILEGES";
-
-cd ~/frappe-bench || exit
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "FLUSH PRIVILEGES";
 
 echo "Setting Up Procfile..."
 
@@ -42,10 +36,11 @@ sed -i 's/^schedule:/# schedule:/g' Procfile
 echo "Starting Bench..."
 
 bench start &> bench_start.log &
+
 CI=Yes bench build &
+build_pid=$!
+
 bench --site lms.test reinstall --yes
-
-bench get-app hrms
-
 bench --site lms.test install-app lms
-bench setup requirements --dev
+
+wait $build_pid
