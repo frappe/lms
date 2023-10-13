f27eecce1fcf639570be8f87c6277154cda5fabf
Jannat Patel <pateljannat2308@gmail.com>
2023-10-13 16:17:58 +0530

    ci: added collation server for mariadb

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -2,6 +2,10 @@
 set -e
 cd ~ || exit
 
+sudo apt update
+sudo apt remove mysql-server mysql-client
+sudo apt install libcups2-dev redis-server mariadb-client-10.6
+
 echo "Setting Up Bench..."
 
 pip install frappe-bench
