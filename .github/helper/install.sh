f2432d78ee3cf98ff1ad0f0f1f22feb2aa1d5cf5
Jannat Patel <pateljannat2308@gmail.com>
2023-10-13 16:29:47 +0530

    ci: added collation server for mariadb

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -23,14 +23,16 @@ mkdir ~/frappe-bench/sites/lms.test
 cp "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/lms.test/site_config.json
 
 
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL character_set_server = 'utf8mb4'";
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL character_set_server = 'utf8mb4'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
 
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE DATABASE test_lms";
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE USER 'test_lms'@'localhost' IDENTIFIED BY 'test_lms'";
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "GRANT ALL PRIVILEGES ON \`test_lms\`.* TO 'test_lms'@'localhost'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE DATABASE test_lms";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE USER 'test_lms'@'localhost' IDENTIFIED BY 'test_lms'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "GRANT ALL PRIVILEGES ON \`test_lms\`.* TO 'test_lms'@'localhost'";
 
-mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "FLUSH PRIVILEGES";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "FLUSH PRIVILEGES";
+
+cd ~/frappe-bench || exit
 
 echo "Setting Up Procfile..."
 
@@ -40,11 +42,10 @@ sed -i 's/^schedule:/# schedule:/g' Procfile
 echo "Starting Bench..."
 
 bench start &> bench_start.log &
-
 CI=Yes bench build &
-build_pid=$!
-
 bench --site lms.test reinstall --yes
-bench --site lms.test install-app lms
 
-wait $build_pid
+bench get-app hrms
+
+bench --site lms.test install-app lms
+bench setup requirements --dev
