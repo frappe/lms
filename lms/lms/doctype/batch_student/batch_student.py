5e569ab0e6d8b03a3a441cb19542f9f429929227
Jannat Patel <pateljannat2308@gmail.com>
2024-01-19 23:56:48 +0530

    feat: self enrollment in batches

--- lms/lms/doctype/batch_student/batch_student.py
+++ lms/lms/doctype/batch_student/batch_student.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2022, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BatchStudent(Document):
 	pass
+
+
+@frappe.whitelist()
+def enroll_batch(batch_name):
+	enrollment = frappe.new_doc("Batch Student")
+	enrollment.student = frappe.session.user
+	enrollment.parent = batch_name
+	enrollment.parentfield = "students"
+	enrollment.parenttype = "LMS Batch"
+	enrollment.save(ignore_permissions=True)
