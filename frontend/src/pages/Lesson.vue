17d4973ab814aac92dcf3689c300f99ece803d0c
Jannat Patel <pateljannat2308@gmail.com>
2024-09-16 18:41:25 +0530

    test: fixed course creation

--- frontend/src/pages/Lesson.vue
+++ frontend/src/pages/Lesson.vue
@@ -244,7 +244,7 @@ const lesson = createResource({
 	onSuccess(data) {
 		lessonProgress.value = data.membership?.progress
 		if (data.content) editor.value = renderEditor('editor', data.content)
-		if (JSON.parse(data.instructor_content)?.blocks?.length)
+		if (JSON.parse(data.instructor_content)?.blocks?.length > 1)
 			instructorEditor.value = renderEditor(
 				'instructor-content',
 				data.instructor_content
