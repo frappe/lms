675bcc8956cbaa665568eab480c5e1693a62bc20
Jannat Patel <pateljannat2308@gmail.com>
2024-10-10 16:20:53 +0530

    test: replaced FrappeTestCase with UnitTestCase

--- lms/lms/doctype/lms_batch_timetable/test_lms_batch_timetable.py
+++ lms/lms/doctype/lms_batch_timetable/test_lms_batch_timetable.py
@@ -2,8 +2,8 @@
 # See license.txt
 
 # import frappe
-from frappe.tests.utils import FrappeTestCase
+import unittest
 
 
-class TestLMSBatchTimetable(FrappeTestCase):
+class TestLMSBatchTimetable(unittest.TestCase):
 	pass
