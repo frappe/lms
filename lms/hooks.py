9593338c7ae86b61bf9f8e7abd7e49ec822d53f3
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2024-12-19 11:30:21 +0530

    export fixtures

--- lms/hooks.py
+++ lms/hooks.py
@@ -119,7 +119,7 @@ scheduler_events = {
 	"daily": ["lms.job.doctype.job_opportunity.job_opportunity.update_job_openings"],
 }
 
-fixtures = ["Custom Field", "Function", "Industry", "LMS Category"]
+fixtures = ["Custom Field", "Function", "Industry", "LMS Category","Custom HTML Block"]
 
 # Testing
 # -------
