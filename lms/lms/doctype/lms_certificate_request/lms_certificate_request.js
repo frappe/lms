4149fa6ce44565ac8d3a3ff97a257baf25d1c318
Jannat Patel <pateljannat2308@gmail.com>
2025-03-04 17:38:43 +0530

    fix: renamed evaluation and certification buttons

--- lms/lms/doctype/lms_certificate_request/lms_certificate_request.js
+++ lms/lms/doctype/lms_certificate_request/lms_certificate_request.js
@@ -4,15 +4,12 @@
 frappe.ui.form.on("LMS Certificate Request", {
 	refresh: function (frm) {
 		if (!frm.is_new() && frm.doc.status == "Upcoming") {
-			frm.add_custom_button(
-				__("Create LMS Certificate Evaluation"),
-				() => {
-					frappe.model.open_mapped_doc({
-						method: "lms.lms.doctype.lms_certificate_request.lms_certificate_request.create_lms_certificate_evaluation",
-						frm: frm,
-					});
-				}
-			);
+			frm.add_custom_button(__("Conduct Evaluation"), () => {
+				frappe.model.open_mapped_doc({
+					method: "lms.lms.doctype.lms_certificate_request.lms_certificate_request.create_lms_certificate_evaluation",
+					frm: frm,
+				});
+			});
 		}
 		if (!frm.doc.google_meet_link && frm.doc.status == "Upcoming") {
 			frm.add_custom_button(__("Generate Google Meet Link"), () => {
