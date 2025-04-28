c1fde95cfa23857b909038fda1e186b3cb3197e1
Sneha-ss-17 <snehas@examroom.ai>
2025-04-28 15:01:11 +0530

    Trainer visibility section added

--- lms/lms/doctype/lms_course/lms_course.py
+++ lms/lms/doctype/lms_course/lms_course.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021, Frappe and contributors
 # For license information, please see license.txt
-
+ 
 import json
 import random
 import frappe
@@ -9,200 +9,226 @@ from frappe.utils import today, cint
 from lms.lms.utils import get_chapters
 from ...utils import generate_slug, validate_image, update_payment_record
 from frappe import _
-
-
+ 
+ 
 class LMSCourse(Document):
-	def validate(self):
-		self.validate_published()
-		self.validate_instructors()
-		self.validate_video_link()
-		self.validate_status()
-		self.validate_payments_app()
-		self.validate_certification()
-		self.validate_amount_and_currency()
-		self.image = validate_image(self.image)
-
-	def validate_published(self):
-		if self.published and not self.published_on:
-			self.published_on = today()
-
-	def validate_instructors(self):
-		if self.is_new() and not self.instructors:
-			frappe.get_doc(
-				{
-					"doctype": "Course Instructor",
-					"instructor": self.owner,
-					"parent": self.name,
-					"parentfield": "instructors",
-					"parenttype": "LMS Course",
-				}
-			).save(ignore_permissions=True)
-
-	def validate_video_link(self):
-		if self.video_link and "/" in self.video_link:
-			self.video_link = self.video_link.split("/")[-1]
-
-	def validate_status(self):
-		if self.published:
-			self.status = "Approved"
-
-	def validate_payments_app(self):
-		if self.paid_course:
-			installed_apps = frappe.get_installed_apps()
-			if "payments" not in installed_apps:
-				documentation_link = "https://docs.frappe.io/learning/setting-up-payment-gateway"
-				frappe.throw(
-					_(
-						"Please install the Payments App to create a paid course. Refer to the documentation for more details. {0}"
-					).format(documentation_link)
-				)
-
-	def validate_certification(self):
-		if self.enable_certification and self.paid_certificate:
-			frappe.throw(
-				_("A course cannot have both paid certificate and certificate of completion.")
-			)
-
-		if self.paid_certificate and not self.evaluator:
-			frappe.throw(_("Evaluator is required for paid certificates."))
-
-	def validate_amount_and_currency(self):
-		if self.paid_course and (cint(self.course_price) < 0 or not self.currency):
-			frappe.throw(_("Amount and currency are required for paid courses."))
-
-		if self.paid_certificate and (cint(self.course_price) <= 0 or not self.currency):
-			frappe.throw(_("Amount and currency are required for paid certificates."))
-
-	def on_update(self):
-		if not self.upcoming and self.has_value_changed("upcoming"):
-			self.send_email_to_interested_users()
-
-	def on_payment_authorized(self, payment_status):
-		if payment_status in ["Authorized", "Completed"]:
-			update_payment_record("LMS Course", self.name)
-
-	def send_email_to_interested_users(self):
-		interested_users = frappe.get_all(
-			"LMS Course Interest", {"course": self.name}, ["name", "user"]
-		)
-		subject = self.title + " is available!"
-		args = {
-			"title": self.title,
-			"course_link": f"/lms/courses/{self.name}",
-			"app_name": frappe.db.get_single_value("System Settings", "app_name"),
-			"site_url": frappe.utils.get_url(),
-		}
-
-		for user in interested_users:
-			args["first_name"] = frappe.db.get_value("User", user.user, "first_name")
-			email_args = frappe._dict(
-				recipients=user.user,
-				subject=subject,
-				header=[subject, "green"],
-				template="lms_course_interest",
-				args=args,
-				now=True,
-			)
-			frappe.enqueue(
-				method=frappe.sendmail, queue="short", timeout=300, is_async=True, **email_args
-			)
-			frappe.db.set_value("LMS Course Interest", user.name, "email_sent", True)
-
-	def autoname(self):
-		if not self.name:
-			self.name = generate_slug(self.title, "LMS Course")
-
-	def __repr__(self):
-		return f"<Course#{self.name}>"
-
-	def has_mentor(self, email):
-		"""Checks if this course has a mentor with given email."""
-		if not email or email == "Guest":
-			return False
-
-		mapping = frappe.get_all(
-			"LMS Course Mentor Mapping", {"course": self.name, "mentor": email}
-		)
-		return mapping != []
-
-	def add_mentor(self, email):
-		"""Adds a new mentor to the course."""
-		if not email:
-			raise ValueError("Invalid email")
-		if email == "Guest":
-			raise ValueError("Guest user can not be added as a mentor")
-
-		# given user is already a mentor
-		if self.has_mentor(email):
-			return
-
-		doc = frappe.get_doc(
-			{"doctype": "LMS Course Mentor Mapping", "course": self.name, "mentor": email}
-		)
-		doc.insert()
-
-	def get_student_batch(self, email):
-		"""Returns the batch the given student is part of.
-
-		Returns None if the student is not part of any batch.
-		"""
-		if not email:
-			return
-
-		batch_name = frappe.get_value(
-			doctype="LMS Enrollment",
-			filters={"course": self.name, "member_type": "Student", "member": email},
-			fieldname="batch_old",
-		)
-		return batch_name and frappe.get_doc("LMS Batch Old", batch_name)
-
-	def get_batches(self, mentor=None):
-		batches = frappe.get_all("LMS Batch Old", {"course": self.name})
-		if mentor:
-			# TODO: optimize this
-			memberships = frappe.db.get_all("LMS Enrollment", {"member": mentor}, ["batch_old"])
-			batch_names = {m.batch_old for m in memberships}
-			return [b for b in batches if b.name in batch_names]
-
-	def get_cohorts(self):
-		return frappe.get_all(
-			"Cohort",
-			{"course": self.name},
-			["name", "slug", "title", "begin_date", "end_date"],
-			order_by="creation",
-		)
-
-	def get_cohort(self, cohort_slug):
-		name = frappe.get_value("Cohort", {"course": self.name, "slug": cohort_slug})
-		return name and frappe.get_doc("Cohort", name)
-
-	def reindex_exercises(self):
-		for i, c in enumerate(get_chapters(self.name), start=1):
-			self._reindex_exercises_in_chapter(c, i)
-
-	def _reindex_exercises_in_chapter(self, c, index):
-		i = 1
-		for lesson in self.get_lessons(c):
-			for exercise in lesson.get_exercises():
-				exercise.index_ = i
-				exercise.index_label = f"{index}.{i}"
-				exercise.save()
-				i += 1
-
-	def get_all_memberships(self, member):
-		all_memberships = frappe.get_all(
-			"LMS Enrollment", {"member": member, "course": self.name}, ["batch_old"]
-		)
-		for membership in all_memberships:
-			membership.batch_title = frappe.db.get_value(
-				"LMS Batch Old", membership.batch_old, "title"
-			)
-		return all_memberships
-
-
+    def validate(self):
+        self.validate_published()
+        self.validate_instructors()
+        self.validate_video_link()
+        self.validate_status()
+        self.validate_payments_app()
+        self.validate_certification()
+        self.validate_amount_and_currency()
+        self.image = validate_image(self.image)
+ 
+    def validate_published(self):
+        if self.published and not self.published_on:
+            self.published_on = today()
+ 
+    def validate_instructors(self):
+        if self.is_new() and not self.instructors:
+            frappe.get_doc(
+                {
+                    "doctype": "Course Instructor",
+                    "instructor": self.owner,
+                    "parent": self.name,
+                    "parentfield": "instructors",
+                    "parenttype": "LMS Course",
+                }
+            ).save(ignore_permissions=True)
+ 
+    def validate_video_link(self):
+        if self.video_link and "/" in self.video_link:
+            self.video_link = self.video_link.split("/")[-1]
+ 
+    def validate_status(self):
+        if self.published:
+            self.status = "Approved"
+ 
+    def validate_payments_app(self):
+        if self.paid_course:
+            installed_apps = frappe.get_installed_apps()
+            if "payments" not in installed_apps:
+                frappe.throw(_("Please install the Payments app to create a paid courses."))
+ 
+    def validate_certification(self):
+        if self.enable_certification and self.paid_certificate:
+            frappe.throw(
+                _("A course cannot have both paid certificate and certificate of completion.")
+            )
+ 
+        if self.paid_certificate and not self.evaluator:
+            frappe.throw(_("Evaluator is required for paid certificates."))
+ 
+    def validate_amount_and_currency(self):
+        if self.paid_course and (cint(self.course_price) < 0 or not self.currency):
+            frappe.throw(_("Amount and currency are required for paid courses."))
+ 
+        if self.paid_certificate and (cint(self.course_price) <= 0 or not self.currency):
+            frappe.throw(_("Amount and currency are required for paid certificates."))
+ 
+    def on_update(self):
+        if not self.upcoming and self.has_value_changed("upcoming"):
+            self.send_email_to_interested_users()
+ 
+    def on_payment_authorized(self, payment_status):
+        if payment_status in ["Authorized", "Completed"]:
+            update_payment_record("LMS Course", self.name)
+ 
+    def send_email_to_interested_users(self):
+        interested_users = frappe.get_all(
+            "LMS Course Interest", {"course": self.name}, ["name", "user"]
+        )
+        subject = self.title + " is available!"
+        args = {
+            "title": self.title,
+            "course_link": f"/lms/courses/{self.name}",
+            "app_name": frappe.db.get_single_value("System Settings", "app_name"),
+            "site_url": frappe.utils.get_url(),
+        }
+ 
+        for user in interested_users:
+            args["first_name"] = frappe.db.get_value("User", user.user, "first_name")
+            email_args = frappe._dict(
+                recipients=user.user,
+                subject=subject,
+                header=[subject, "green"],
+                template="lms_course_interest",
+                args=args,
+                now=True,
+            )
+            frappe.enqueue(
+                method=frappe.sendmail, queue="short", timeout=300, is_async=True, **email_args
+            )
+            frappe.db.set_value("LMS Course Interest", user.name, "email_sent", True)
+ 
+    def autoname(self):
+        if not self.name:
+            self.name = generate_slug(self.title, "LMS Course")
+ 
+    def __repr__(self):
+        return f"<Course#{self.name}>"
+ 
+    def has_mentor(self, email):
+        """Checks if this course has a mentor with given email."""
+        if not email or email == "Guest":
+            return False
+ 
+        mapping = frappe.get_all(
+            "LMS Course Mentor Mapping", {"course": self.name, "mentor": email}
+        )
+        return mapping != []
+ 
+    def add_mentor(self, email):
+        """Adds a new mentor to the course."""
+        if not email:
+            raise ValueError("Invalid email")
+        if email == "Guest":
+            raise ValueError("Guest user can not be added as a mentor")
+ 
+        # given user is already a mentor
+        if self.has_mentor(email):
+            return
+ 
+        doc = frappe.get_doc(
+            {"doctype": "LMS Course Mentor Mapping", "course": self.name, "mentor": email}
+        )
+        doc.insert()
+ 
+    def get_student_batch(self, email):
+        """Returns the batch the given student is part of.
+ 
+        Returns None if the student is not part of any batch.
+        """
+        if not email:
+            return
+ 
+        batch_name = frappe.get_value(
+            doctype="LMS Enrollment",
+            filters={"course": self.name, "member_type": "Student", "member": email},
+            fieldname="batch_old",
+        )
+        return batch_name and frappe.get_doc("LMS Batch Old", batch_name)
+ 
+    def get_batches(self, mentor=None):
+        batches = frappe.get_all("LMS Batch Old", {"course": self.name})
+        if mentor:
+            # TODO: optimize this
+            memberships = frappe.db.get_all("LMS Enrollment", {"member": mentor}, ["batch_old"])
+            batch_names = {m.batch_old for m in memberships}
+            return [b for b in batches if b.name in batch_names]
+ 
+    def get_cohorts(self):
+        return frappe.get_all(
+            "Cohort",
+            {"course": self.name},
+            ["name", "slug", "title", "begin_date", "end_date"],
+            order_by="creation",
+        )
+ 
+    def get_cohort(self, cohort_slug):
+        name = frappe.get_value("Cohort", {"course": self.name, "slug": cohort_slug})
+        return name and frappe.get_doc("Cohort", name)
+ 
+    def reindex_exercises(self):
+        for i, c in enumerate(get_chapters(self.name), start=1):
+            self._reindex_exercises_in_chapter(c, i)
+ 
+    def _reindex_exercises_in_chapter(self, c, index):
+        i = 1
+        for lesson in self.get_lessons(c):
+            for exercise in lesson.get_exercises():
+                exercise.index_ = i
+                exercise.index_label = f"{index}.{i}"
+                exercise.save()
+                i += 1
+ 
+    def get_all_memberships(self, member):
+        all_memberships = frappe.get_all(
+            "LMS Enrollment", {"member": member, "course": self.name}, ["batch_old"]
+        )
+        for membership in all_memberships:
+            membership.batch_title = frappe.db.get_value(
+                "LMS Batch Old", membership.batch_old, "title"
+            )
+        return all_memberships
+ 
+ 
 @frappe.whitelist()
 def reindex_exercises(doc):
-	course_data = json.loads(doc)
-	course = frappe.get_doc("LMS Course", course_data["name"])
-	course.reindex_exercises()
-	frappe.msgprint("All exercises in this course have been re-indexed.")
+    course_data = json.loads(doc)
+    course = frappe.get_doc("LMS Course", course_data["name"])
+    course.reindex_exercises()
+    frappe.msgprint("All exercises in this course have been re-indexed.")
+ 
+ 
+# Trainer Visibility 
+ 
+@frappe.whitelist()
+def get_permission_query_conditions(user):
+    if "Administrator" in frappe.get_roles(user):
+        return None  # Admin sees everything
+ 
+    # LMS Trainer: only their own courses
+    if "LMS Trainer" in frappe.get_roles(user):
+        return f"`tabLMS Course`.owner = '{user}'"
+ 
+    return None
+ 
+def has_permission(doc, ptype, user):
+    if getattr(doc, "__islocal", False):
+        return True  # allow unsaved
+ 
+    if user == "Administrator":
+        return True
+ 
+    if ptype == "read":
+        # Trainer can only read their own courses
+        if "LMS Trainer" in frappe.get_roles(user):
+            return doc.owner == user
+ 
+    if ptype in ["write", "submit", "cancel", "delete"]:
+        return doc.owner == user or user == "Administrator"
+ 
+    return False
