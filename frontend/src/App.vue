21708191597db2ad195ad05e938e07d17223c4bf
Jannat Patel <pateljannat2308@gmail.com>
2025-05-26 22:02:46 +0530

    chore: telemetry fixes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,16 +9,19 @@
 <script setup>
 import { FrappeUIProvider } from 'frappe-ui'
 import { Dialogs } from '@/utils/dialogs'
-import { computed, onUnmounted, ref } from 'vue'
+import { computed, onUnmounted, ref, watch } from 'vue'
 import { useScreenSize } from './utils/composables'
 import DesktopLayout from './components/DesktopLayout.vue'
 import MobileLayout from './components/MobileLayout.vue'
 import NoSidebarLayout from './components/NoSidebarLayout.vue'
+import { usersStore } from '@/stores/user'
 import { useRouter } from 'vue-router'
+import { posthogSettings } from '@/telemetry'
 
 const screenSize = useScreenSize()
 const router = useRouter()
 const noSidebar = ref(false)
+const { userResource } = usersStore()
 
 router.beforeEach((to, from, next) => {
 	if (to.query.fromLesson || to.path === '/persona') {
@@ -43,5 +46,11 @@ const Layout = computed(() => {
 onUnmounted(() => {
 	noSidebar.value = false
 	stopSession()
+})
+
+watch(userResource, () => {
+	if (userResource.data) {
+		posthogSettings.reload()
+	}
 })
 </script>
