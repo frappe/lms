a9dcc8e58f19bb76cfe02dbdcc1da499f57c2e14
sinardyas <sinardyas96@gmail.com>
2025-09-05 19:46:11 +0700

    Add login and template files; update Welcome page layout and styles

--- frontend/src/pages/Welcome.vue
+++ frontend/src/pages/Welcome.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="h-screen w-full flex flex-col gap-8 items-center justify-center text-white px-12">
-        <div class="text-center flex gap-4 bg-gradient-to-tr from-blue-800 to-blue-400 p-32 rounded-lg">
+    <!-- <PdfViewer :pdfUrl="'http://lms.test:8080/test.pdf'" /> -->
+    <div class="sm:h-screen w-full flex flex-col gap-8 items-center justify-center text-white px-4 md:px-8 lg:px-12 py-4">
+        <div class="text-center flex flex-col md:flex-row items-center gap-4 bg-gradient-to-tr from-blue-800 to-blue-400 p-4 md:p-14 lg:p-32 rounded-lg">
             <div class="flex flex-col gap-4">
-                <p class="text-2xl font-bold">
+                <p class="text-lg md:text-xl lg:text-2xl font-semibold">
                     I'm excited to introduce our new Competency Management System, a direct investment in your growth. This platform will provide clarity on the skills we value and a clear roadmap for your career development. I encourage you to explore it and actively shape your future with us.
     Welcome to this new chapter.
                 </p>
 
-                <p class="text-md font-bold">
+                <p class="text-sm sm:text-md font-bold">
                     Pieters Utomo - CEO Soechi Lines
                 </p>
             </div>
-            <img :src="CeoImg" class="w-40 rounded-full"/>
+            <img :src="CeoImg" class="w-32 md:w-40 rounded-full"/>
         </div>
 
-        <div class="text-center flex gap-4 bg-gradient-to-tr from-blue-400 to-blue-800 py-32 px-24 rounded-lg">
-            <img :src="DirectorImg" class="w-40 rounded-full"/>
+        <div class="text-center flex flex-col md:flex-row items-center gap-4 bg-gradient-to-tr from-blue-400 to-blue-800 p-4 md:p-14 lg:p-32 rounded-lg">
+            <img :src="DirectorImg" class="w-32 md:w-40 rounded-full"/>
             <div class="flex flex-col gap-4">
-                <p class="text-2xl font-bold">
+                <p class="text-lg md:text-xl lg:text-2xl font-semibold">
                     I am pleased to announce the launch of our new Competency Management System, designed to align skills with organizational goals and support continuous professional growth. I encourage all employees to actively engage with this platform as we work together to build a stronger, future-ready organization.
                 </p>
     
-                <p class="text-md font-bold">
+                <p class="text-sm sm:text-md font-bold">
                     Capt. Karan Sawhney
-                    Director – Strategic Development & Assurance at Prima Energi & Soechi Group
+                    Director – Strategic Development & Assurance Soechi Lines
                 </p>
             </div>
         </div>
@@ -33,6 +34,7 @@
 import { computed, inject, watch, ref, onMounted, watchEffect } from 'vue'
 import CeoImg from '@/assets/ceo.jpeg';
 import DirectorImg from '@/assets/director.jpeg';
+import PdfViewer from '@/components/PdfViewer.vue';
 
 onMounted(() => {
     console.log(">>> Mounted called");
