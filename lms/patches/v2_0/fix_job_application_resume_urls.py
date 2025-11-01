a5f9adc8758856e2b6bf234d2dd4af7da70c7a95
Rehan Ansari <rehanrehman389@gmail.com>
2025-11-01 14:48:45 +0530

    fix: patch old resumes to be private, link to job application, and share with job owner

--- lms/patches/v2_0/fix_job_application_resume_urls.py
+++ lms/patches/v2_0/fix_job_application_resume_urls.py
@@ -2,13 +2,30 @@ import frappe
 
 
 def execute():
-	applications = frappe.get_all("LMS Job Application", fields=["name", "resume"])
+	applications = frappe.get_all(
+		"LMS Job Application",
+		fields=["name", "resume", "job.owner as job_owner"],
+		filters={"resume": ["not like", "/files/%"]},
+	)
 
 	for application in applications:
-		if application.resume and not application.resume.startswith("/files/"):
-			file_doc = frappe.db.get_value(
-				"File", {"file_name": application.resume}, ["file_url", "name"], as_dict=True
-			)
+		if not application.resume:
+			continue
 
-			if file_doc and file_doc.file_url:
-				frappe.db.set_value("LMS Job Application", application.name, "resume", file_doc.file_url)
+		file_name = frappe.db.get_value(
+			"File", {"file_name": application.resume, "attached_to_name": ["is", "not set"]}, "name"
+		)
+
+		if file_name:
+			file_doc = frappe.get_doc("File", file_name)
+
+			file_doc.is_private = 1
+			file_doc.attached_to_doctype = "LMS Job Application"
+			file_doc.attached_to_name = application.name
+			file_doc.attached_to_field = "resume"
+			file_doc.save()
+
+			if application.job_owner:
+				frappe.share.add_docshare(
+					"LMS Job Application", application.name, application.job_owner, read=1
+				)
