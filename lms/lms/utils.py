cc38227d14c9e36238a5653f0c83102e47174e19
Anugrah <anugrahsingh3108@gmail.com>
2025-12-30 13:45:28 +0530

    Revert "refactor: switched field definitions from dict-style aggregation to explicit SQL with aliases for predictable column names and readability."
    This reverts commit 2396259132c3bf8af549041a75bf36cb73ca0e0e.

--- lms/lms/utils.py
+++ lms/lms/utils.py
@@ -551,7 +551,7 @@ def get_chart_data(
 
 	data = frappe.db.get_all(
 		doctype,
-		fields=[f"{datefield} as _unit", f"SUM({value_field}) as total", "COUNT(*) as count"],
+		fields=[datefield, {"SUM": value_field}, {"COUNT": "*"}],
 		filters=filters,
 		group_by=datefield,
 		order_by=datefield,
