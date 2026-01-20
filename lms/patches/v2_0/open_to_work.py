afbdb46fe8dbf854f0ed34ab0dcc6c190b4f31ed
Jannat Patel <pateljannat2308@gmail.com>
2026-01-20 15:47:55 +0530

    fix: open to work patch

--- lms/patches/v2_0/open_to_work.py
+++ lms/patches/v2_0/open_to_work.py
@@ -6,3 +6,7 @@ def execute():
 
 	if not open_to_field_exists:
 		return
+
+	open_to_opportunities = frappe.get_all("User", {"open_to": "Opportunities"}, ["name"])
+	for user in open_to_opportunities:
+		frappe.db.set_value("User", user.name, "open_to", "Work")
