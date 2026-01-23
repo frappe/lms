aeb2724e82e5d2cdcc550c2c9ed0ec13852c325e
raizasafeel <89463672+raizasafeel@users.noreply.github.com>
2026-01-23 12:26:03 +0530

    test: use BaseTestUtils in test_auth for consistent test setup

--- lms/test_auth.py
+++ lms/test_auth.py
@@ -1,13 +1,13 @@
 import frappe
 from frappe.tests.test_api import FrappeAPITestCase
 
+from lms.auth import authenticate
 from lms.lms.test_helpers import BaseTestUtils
 
 
-class TestAuth(FrappeAPITestCase, BaseTestUtils):
+class TestAuth(BaseTestUtils, FrappeAPITestCase):
 	def setUp(self):
 		super().setUp()
-		BaseTestUtils.setUp(self)
 		self.normal_user = self._create_user("normal-user@example.com", "Normal", "User", ["LMS Student"])
 
 	def test_allowed_path(self):
@@ -23,4 +23,4 @@ class TestAuth(FrappeAPITestCase, BaseTestUtils):
 		frappe.session.user = "Administrator"
 
 	def tearDown(self):
-		BaseTestUtils.tearDown(self)
+		super().tearDown()
