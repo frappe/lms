214c723a557dc96f56217dd7c91aac3b536d86cf
Advaith Saji <advaith.saji@qburst.com>
2026-05-18 22:40:21 +0530

    fix: resolved race condition for get certificate button

--- frontend/src/components/CourseCardOverlay.vue
+++ frontend/src/components/CourseCardOverlay.vue
@@ -77,18 +77,20 @@
 						{{ __('Start Learning') }}
 					</span>
 				</Button>
-				<Button
-					v-if="canGetCertificate"
-					@click="fetchCertificate()"
-					variant="subtle"
-					class="w-full mt-2"
-					size="md"
-				>
-					<template #prefix>
-						<GraduationCap class="size-4 stroke-1.5" />
-					</template>
-					{{ __('Get Certificate') }}
-				</Button>
+			<Button
+				v-if="canGetCertificate"
+				@click="fetchCertificate()"
+				:disabled="certificate.loading"
+				:loading="certificate.loading"
+				variant="subtle"
+				class="w-full mt-2"
+				size="md"
+			>
+				<template #prefix>
+					<GraduationCap class="size-4 stroke-1.5" />
+				</template>
+				{{ __('Get Certificate') }}
+			</Button>
 			</div>
 			<div class="space-y-3">
 				<div class="font-medium text-ink-gray-9">
@@ -255,6 +257,7 @@ const certificate = createResource({
 })
 
 const fetchCertificate = () => {
+	if (certificate.loading) return
 	certificate.submit({
 		course: props.course.data?.name,
 		member: user.data?.name,
