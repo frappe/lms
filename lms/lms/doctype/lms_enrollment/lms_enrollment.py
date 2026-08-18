cd42e6afedf49dbb4f647b74251fbcae14a66283
Abdullah <abdullahansari.dbit@gmail.com>
2026-08-18 05:34:16 +0000

    fix: on update fix

--- lms/lms/doctype/lms_enrollment/lms_enrollment.py
+++ lms/lms/doctype/lms_enrollment/lms_enrollment.py
@@ -35,10 +35,11 @@ class LMSEnrollment(Document):
 		if self.owner != self.member:
 			self.owner = self.member
 
+	def on_update(self):
+		update_program_progress(self.member)
+
 	def after_insert(self):
 		from lms.lms.doctype.lms_course.lms_course import update_course_enrollments
-
-		update_program_progress(self.member)
 
 		if self.member_type == "Student":
 			update_course_enrollments(self.course)
